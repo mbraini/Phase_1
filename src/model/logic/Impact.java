797f371468f6a4afcf5534e960255883fddc4334
Mahdi Barani <mahdi.mb.1384@gmail.com>
2024-04-18 10:34:50 -0700

    Compeleting Impact Without Dash Animation

--- src/model/logic/Impact.java
+++ src/model/logic/Impact.java
@@ -1,4 +1,35 @@
 package model.logic;
 
+import controller.Constants;
+import controller.animations.DashAnimation;
+import controller.helper.Utils;
+import controller.helper.Vector;
+import model.interfaces.EpsilonGravity;
+import model.objectsModel.OIGModel;
+
 public class Impact {
+    Vector collisionPoint;
+
+    public Impact(Vector collisionPoint){
+        this.collisionPoint = collisionPoint;
+    }
+
+    public void MakeImpact(){
+        double distance;
+        for (int i = 0 ;i < OIGModel.OIGs.size() ;i++){
+            Vector direction;
+            if (OIGModel.OIGs.get(i) instanceof EpsilonGravity){
+                ((EpsilonGravity) OIGModel.OIGs.get(i)).setVisibility(false);
+            }
+            direction = Utils.VectorAdd(Utils.ScalarInVector(-1 ,collisionPoint) ,OIGModel.OIGs.get(i).getPosition());
+            distance = Utils.VectorSize(direction);
+            //////////////////todo
+            if (distance >= 500){
+                return;
+            }
+            //////////////////todo
+            new DashAnimation(OIGModel.OIGs.get(i) ,direction).StartAnimation();
+        }
+    }
+
 }
