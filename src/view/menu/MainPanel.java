b054a99f36f1a121c613b366ccf6ffee773b36de
Mahdi Barani <mahdi.mb.1384@gmail.com>
2024-04-18 09:35:27 -0700

    Compeleting MainPanel

--- src/view/menu/MainPanel.java
+++ src/view/menu/MainPanel.java
@@ -1,4 +1,25 @@
 package view.menu;
 
-public class MainPanel {
+import controller.Constants;
+import view.PIG;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainPanel extends PIG {
+    public MainPanel(){
+        this.setLayout(null);
+        this.setBounds(0,0, Constants.GAME_WIDTH, Constants.GAME_HEIGHT);
+        this.setBackground(Color.BLACK);
+        this.setVisible(true);
+    }
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void end() {
+
+    }
 }
