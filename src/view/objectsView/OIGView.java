1d131329f52b16cfa01120f03421de3eaaeedfc6
Mahdi Barani <mahdi.mb.1384@gmail.com>
2024-04-18 09:22:26 -0700

    Adding OIGView Logic

--- src/view/objectsView/OIGView.java
+++ src/view/objectsView/OIGView.java
@@ -1,4 +1,40 @@
 package view.objectsView;
 
-public class OIGView {
+import controller.helper.Vector;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public abstract class OIGView {
+    String id;
+    Vector position;
+    double theta;
+    public static ArrayList<OIGView> OIGs = new ArrayList<>();
+
+    abstract public void draw(Graphics2D g2d);
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Vector getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vector position) {
+        this.position = position;
+    }
+
+    public double getTheta() {
+        return theta;
+    }
+
+    public void setTheta(double theta) {
+        this.theta = theta;
+    }
+
 }
