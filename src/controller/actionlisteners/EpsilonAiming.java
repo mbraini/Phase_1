8d0af0cd85df49cd84180c85d7d324491e0421e0
Mahdi Barani <mahdi.mb.1384@gmail.com>
2024-04-18 09:54:27 -0700

    Compeleting EpsilonAiming

--- src/controller/actionlisteners/EpsilonAiming.java
+++ src/controller/actionlisteners/EpsilonAiming.java
@@ -1,4 +1,56 @@
 package controller.actionlisteners;
 
-public class EpsilonAiming {
+import controller.Constants;
+import controller.GameState;
+import controller.Spawn;
+import controller.helper.Utils;
+import controller.helper.Vector;
+import model.objectsModel.EpsilonModel;
+import model.objectsModel.OIGModel;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class EpsilonAiming implements MouseListener {
+
+    double timer;
+    public EpsilonAiming(){
+        timer = 0;
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if ((GameState.time - timer) * 1000 >= Constants.AIMING_PAUSE_TIME){
+            timer = GameState.time;
+            EpsilonModel epsilon = (EpsilonModel) OIGModel.OIGs.get(0);
+            Vector clickedPoint = new Vector(e.getX() ,e.getY());
+            if (clickedPoint.Equals(epsilon.getPosition()))
+                return;
+            Vector direction = Utils.VectorAdd(Utils.ScalarInVector(-1 ,epsilon.getPosition()) ,clickedPoint);
+            Vector position = new Vector(epsilon.getPosition().x ,epsilon.getPosition().y);
+            position = Utils.VectorAdd(Utils.VectorWithSize(direction ,Constants.BULLET_DIAMETER / 2 + Constants.EPSILON_DIMENSION.width) ,epsilon.getPosition());
+            Spawn.SpawnBullet(position ,direction);
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
