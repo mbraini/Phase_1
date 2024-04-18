652516260655842001160fe66b7f78b17cbec383
Mahdi Barani <mahdi.mb.1384@gmail.com>
2024-04-18 09:43:42 -0700

    Making GameStartAnimation With Timer

--- src/controller/animations/GameStartAnimation.java
+++ src/controller/animations/GameStartAnimation.java
@@ -1,4 +1,42 @@
 package controller.animations;
 
-public class GameStartAnimation {
+import controller.Application;
+import controller.Constants;
+import controller.Spawn;
+import model.objectsModel.EpsilonModel;
+import model.objectsModel.OIGModel;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GameStartAnimation extends Animation{
+    static Timer gameStartAnimation;
+
+    @Override
+    public void StartAnimation(){
+        gameStartAnimation = new Timer(10, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                ////////////////////////////todo
+                for (int i = 0; i < OIGModel.OIGs.size() ; i++){
+                    if (OIGModel.OIGs.get(i) instanceof EpsilonModel){
+                        ((EpsilonModel) OIGModel.OIGs.get(i)).setPosition(((EpsilonModel) OIGModel.OIGs.get(i)).getPosition().x - 2 ,((EpsilonModel) OIGModel.OIGs.get(i)).getPosition().y - 2);
+                    }
+                }
+                ////////////////////////////todo
+                Application.gameFrame.RightAddSize(-2);
+                Application.gameFrame.DownAddSize(-2);
+                Application.gameFrame.LeftAddSize(-2);
+                Application.gameFrame.UpAddSize(-2);
+                if (Application.gameFrame.getWidth() - Constants.barD.width == 600){
+                    OIGModel.OIGs.get(0).setVelocity(0 ,0);
+                    Spawn.SpawnEnemy();
+                    gameStartAnimation.removeActionListener(this);
+                }
+            }
+        });
+        gameStartAnimation.start();
+    }
 }
