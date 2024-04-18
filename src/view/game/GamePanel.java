71e6bdecd81963484bc56a1587b957043915aeb7
Mahdi Barani <mahdi.mb.1384@gmail.com>
2024-04-18 09:45:48 -0700

    Compeleting GamePanel

--- src/view/game/GamePanel.java
+++ src/view/game/GamePanel.java
@@ -1,4 +1,30 @@
 package view.game;
 
-public class GamePanel {
+import view.PIG;
+
+import java.awt.*;
+
+public class GamePanel extends PIG {
+
+    public GamePanel(){
+        this.setLayout(null);
+        this.setBackground(Color.RED);
+        this.setBounds(-1000 ,-1000 ,3000 ,3000);
+    }
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void end() {
+
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+    }
 }
