d9d48b077b9c250e76a69d1ec86b2f4f523075fb
Mahdi Barani <mahdi.mb.1384@gmail.com>
2024-04-25 09:08:19 -0700

    Implementing Trigorath Ability

--- src/model/objectsModel/TrigorathModel.java
+++ src/model/objectsModel/TrigorathModel.java
@@ -1,14 +1,16 @@
 package model.objectsModel;
 
 import controller.Constants;
+import controller.Controller;
 import controller.helper.Utils;
 import controller.helper.Vector;
+import model.interfaces.Ability;
 import model.interfaces.HasVertices;
 import model.interfaces.IsPolygon;
 
 import java.util.ArrayList;
 
-public class TrigorathModel extends EnemyModel implements HasVertices, IsPolygon {
+public class TrigorathModel extends EnemyModel implements HasVertices, IsPolygon , Ability {
     public ArrayList<Vector> vertices;
     boolean epsilonGravityVisibility = true;
     public TrigorathModel(Vector position ,String id){
@@ -54,7 +56,26 @@ public class TrigorathModel extends EnemyModel implements HasVertices, IsPolygon
     }
 
     @Override
+    public void epsilonGravity() {
+        super.epsilonGravity();
+        ability();
+    }
+
+    @Override
     public void setVisibility(boolean visibility) {
         epsilonGravityVisibility = visibility;
+    }
+
+    @Override
+    public void ability() {
+        double distance = Utils.VectorSize(Utils.VectorAdd(Utils.ScalarInVector(-1 ,position) , Controller.getEpsilon().getPosition()));
+        /////////////todo
+        if (distance <= Constants.TRIGORATH_DIMENTION.width + Constants.EPSILON_DIMENSION.width){
+            velocity = Utils.VectorWithSize(velocity ,Constants.ENEMY_LINEAR_SPEED / 4d);
+        }
+        if (distance <= Constants.TRIGORATH_DIMENTION.width + Constants.EPSILON_DIMENSION.width + 50){
+            velocity = Utils.VectorWithSize(velocity ,Constants.ENEMY_LINEAR_SPEED / 3d);
+        }
+        ////////////todo
     }
 }
