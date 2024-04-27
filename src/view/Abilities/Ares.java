559972d5f925ae16ea59d1227e677ca2b1e28c19
Mahdi Barani <mahdi.mb.1384@gmail.com>
2024-04-27 08:49:00 -0700

    Implementing Ares

--- src/view/Abilities/Ares.java
+++ src/view/Abilities/Ares.java
@@ -1,9 +1,11 @@
 package view.Abilities;
 
+import controller.Configs;
+
 public class Ares extends SpecialAbility{
     public static boolean available = true;
     @Override
     public void performAbility() {
-
+        Configs.EXTRA_DAMAGE = 2;
     }
 }
